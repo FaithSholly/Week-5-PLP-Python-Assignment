6e09949a3b9e1be8cc2c7959637f0f1e9989daf6
FaithSholly <aminatomo342@gmail.com>
2025-09-02 18:43:44 +0100

    Create Polymorphism.py

---
+++ Polymorphism.py
@@ -0,0 +1,23 @@
+# Activity 2: Polymorphism Challenge
+
+class Vehicle:
+    def move(self):
+        pass  # Placeholder (to be overridden by child classes)
+
+class Car(Vehicle):
+    def move(self):
+        print("🚗 Driving on the road")
+
+class Plane(Vehicle):
+    def move(self):
+        print("✈️ Flying in the sky")
+
+class Boat(Vehicle):
+    def move(self):
+        print("🚢 Sailing on the water")
+
+# --- Testing Polymorphism ---
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()
